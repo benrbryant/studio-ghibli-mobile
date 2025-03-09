6ce2ef14abd9a826b551c728645dc0fc2541d846
Ben Bryant <brbryant2639@gmail.com>
2025-03-08 22:59:28 -0600

    added default dynamic routes for individual resources

---
+++ app/films/[id].tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import TitleText from "@/common/components/ui/TitleText";
+import { useTheme } from "@/common/hooks/useTheme";
+import { useLocalSearchParams } from "expo-router";
+
+export default function FilmScreen() {
+  const theme = useTheme();
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View
+      style={[
+        {
+          backgroundColor: theme.colors.background,
+        },
+        styles.container,
+      ]}
+    >
+      <TitleText title={`Film ${id}`} type="xl" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+});
