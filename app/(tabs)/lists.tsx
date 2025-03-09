0e5b6cf02212b1751c2c58446855d52ffe74ef85
Ben Bryant <brbryant2639@gmail.com>
2025-03-09 14:00:02 -0500

    added simple text filter for films list

--- app/(tabs)/lists.tsx
+++ app/(tabs)/lists.tsx
@@ -1,17 +1,103 @@
+import { Resource, RESOURCES } from "@/common/constants/resources";
 import FilmList from "@/features/film/components/FilmList";
 import { useLocalSearchParams } from "expo-router/build/hooks";
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
+import { useTheme } from "@/common/hooks/useTheme";
 
 export default function ListsScreen() {
   const { type } = useLocalSearchParams();
+  const theme = useTheme();
+
+  const [selectedType, setSelectedType] = useState(type || "films");
+  const [searchText, setSearchText] = useState("");
+
+  function determineResourceColor(resource: Resource): string {
+    if (selectedType === resource.title.toLowerCase()) {
+      return resource.color;
+    } else {
+      return theme.colors.card;
+    }
+  }
+
+  useEffect(() => {
+    setSelectedType(type);
+  }, [type]);
+
+  const dynamicStyles = {
+    container: {
+      backgroundColor: theme.colors.background,
+    },
+    listBtn: {
+      borderRadius: theme.roundness.lg,
+      backgroundColor: theme.colors.card,
+    },
+    text: {
+      color: theme.colors.text,
+    },
+    searchInput: {
+      color: theme.colors.text,
+      borderRadius: theme.roundness.md,
+    },
+  };
 
   return (
-    <View>
-        {/* Buttons per category */}
-        {/* Sorting */}
-        {/* List */}
-      <Text>Show {type} list</Text>
-      <FilmList />
+    <View style={[styles.container, dynamicStyles.container]}>
+      <View style={styles.listBtnContainer}>
+        {RESOURCES.map((resource) => {
+          return (
+            <Pressable
+              key={resource.id}
+              onPress={() => setSelectedType(resource.title.toLowerCase())}
+              style={[
+                styles.listBtn,
+                dynamicStyles.listBtn,
+                {
+                  borderColor: resource.color,
+                  borderWidth: 1,
+                  backgroundColor: determineResourceColor(resource),
+                },
+              ]}
+            >
+              <Text style={dynamicStyles.text}>{resource.title}</Text>
+            </Pressable>
+          );
+        })}
+      </View>
+      <View style={{ marginTop: 24, marginBottom: 24 }}>
+        <TextInput
+          keyboardType="default"
+          placeholder="Search..."
+          style={[styles.searchInput, dynamicStyles.searchInput]}
+          value={searchText}
+          onChangeText={(text) => setSearchText(text)}
+        />
+      </View>
+      <FilmList searchTerm={searchText.trim().toLowerCase()} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    flex: 1,
+  },
+  listBtnContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 6,
+    marginTop: 16,
+  },
+  listBtn: {
+    padding: 8,
+  },
+  searchInput: {
+    height: 50,
+    borderWidth: 1,
+    padding: 8,
+    fontSize: 16,
+    borderColor: "#FFFFFF",
+  },
+});
--- features/film/components/FilmList.tsx
+++ features/film/components/FilmList.tsx
@@ -1,16 +1,11 @@
 import { ReactNode, ReactElement } from "react";
-import {
-  Text,
-  FlatList,
-  ListRenderItemInfo,
-  View,
-} from "react-native";
+import { Text, FlatList, ListRenderItemInfo, View } from "react-native";
 import useFilms from "../hooks/useFilms";
 import { Film } from "../types";
 import Loader from "@/common/components/ui/Loader";
 import FilmListItem from "./FilmListItem";
 
-export default function FilmList(): ReactNode {
+export default function FilmList({ searchTerm }: FilmListProps): ReactNode {
   const { data, status } = useFilms();
 
   function renderFilmItem({ item }: ListRenderItemInfo<Film>): ReactElement {
@@ -26,12 +21,19 @@ export default function FilmList(): ReactNode {
       </View>
     );
   } else {
+    let filteredData = data.filter((film) =>
+      film.title.toLowerCase().includes(searchTerm)
+    );
     return (
       <FlatList
-        data={data}
+        data={filteredData}
         renderItem={renderFilmItem}
         keyExtractor={(item) => item.id}
       />
     );
   }
 }
+
+type FilmListProps = {
+  searchTerm: string;
+};
